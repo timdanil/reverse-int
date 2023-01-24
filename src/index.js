ce96ec2e1a067eee0129c23c083da4062d57a895
Daniil Timoshenko <timdanil99@gmail.com>
2023-01-24 17:08:16 +1000

    feat:add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    return n > 0 
+    ? Number(String(n).split('').reverse().join('')) 
+    :(Number(String(Math.abs(n)).split('').reverse().join('')))
 }
